ab134107f5631dfacb764d2b3686c0e9a400a53e
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-22 18:25:39 -0300

    fix: hydratation error with toaster

--- src/app/auth/components/auth-form.tsx
+++ src/app/auth/components/auth-form.tsx
@@ -24,6 +24,7 @@ export function AuthForm() {
       toast({
         title: "Error",
         description: "An error occurred. Please try again.",
+        variant: "destructive",
       });
     }
   });
