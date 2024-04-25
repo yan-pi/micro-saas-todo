544c1e93e465b58a0c00df7720d68436608d494c
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-04-25 17:58:38 -0300

    feat: create auth redirect middleware

---
+++ src/lib/get-url.ts
@@ -0,0 +1,5 @@
+export function getUrl(path?: string) {
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  const normalizedPath = path && !path.startsWith("/") ? `/${path}` : path || ""
+  return `${baseUrl}${normalizedPath}`
+}
